b70c892d2a1edd2fb23dcab65385335b7175fcc2
maxwellharon <maxwellharon@gmail.com>
2018-06-24 13:13:25 +0300

    Did imports in the views.py file

---
+++ app/auth/views.py
@@ -0,0 +1,9 @@
+from flask import render_template,redirect,url_for,flash
+from flask_login import login_user,login_required,logout_user
+from . import auth
+from ..models import User
+from .forms import RegistrationForm,LoginForm
+from ..import db
+from ..import bcrypt
+from .. import login_manager
+from sqlalchemy import exc
