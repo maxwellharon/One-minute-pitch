52c3b4da39d2dbdc21d32bd71b2c7280a686e408
maxwellharon <maxwellharon@gmail.com>
2018-06-24 13:07:27 +0300

    Finish on staging Manager.py file

--- manage.py
+++ manage.py
@@ -1,17 +1,29 @@
+from flask_migrate import Migrate,MigrateCommand
+from flask_script import Manager,Server
 
+#local imports
+from app import db,socketio
+from app import create_app
 
+from app.models import *
+from config import *
 
+app = create_app('production')
 
+# manager commands
+manager = Manager(app)
+migrate = Migrate(app,db)
 
+# manager functionalities
+manager.add_command('db',MigrateCommand)
+manager.add_command('runserver',Server)
 
-
-
-
-
+# for shell functionalities.
 @manager.shell
 def make_shell_context():
 	return dict(app=app,db=db)
 
+# testing settings.
 @manager.command
 def test():
 	import unittest
