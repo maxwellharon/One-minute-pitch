1f9db5b98c0d47d8abed71c202d99f1ba1e91795
maxwellharon <maxwellharon@gmail.com>
2018-06-24 13:44:36 +0300

    Created a model that handles the user model that will be mapped to the database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
 from flask_login import UserMixin
 from . import db
 from datetime import datetime
+
+
+class User(UserMixin,db.Model):
+
+	""" This model handles the User model that will be mapped to the database"""
+
+	__tablename__="users"
+	id = db.Column(db.Integer,primary_key=True)
+	username = db.Column(db.String(255),index=True)
+	email = db.Column(db.String(255), unique=True,index=True)
+	bio = db.Column(db.String(255))
+	profile_pic_path=db.Column(db.String())
+	joined = db.Column(db.DateTime,default=datetime.utcnow)
+	password_hash = db.Column(db.String(255))
+	pitch = db.relationship('Pitch',backref='author',lazy='dynamic')
+	comments = db.relationship('Comment',backref='author',lazy='dynamic')
+
+	def __repr__(self):
+		return f"User : {self.username}"
