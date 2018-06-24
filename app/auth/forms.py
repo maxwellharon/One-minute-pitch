7ba1ebc437a15d080c6e650d25eb87058c8d82db
maxwellharon <maxwellharon@gmail.com>
2018-06-24 13:10:58 +0300

    Created a class for Registration form with email,username and password

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,3 +4,12 @@ from wtforms.validators import Required,Email,EqualTo
 from wtforms import ValidationError
 from ..models import User
 from wtforms.fields.html5 import EmailField
+
+
+class RegistrationForm(FlaskForm):
+    email = EmailField('Your Email Address',validators=[Required(),Email(message="Email is required.")])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(),
+    EqualTo('password_confirm',message = 'Passwords do not match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
