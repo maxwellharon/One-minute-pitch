f6782577c526387b7436161ac3f641f1adacdaf3
maxwellharon <maxwellharon@gmail.com>
2018-06-24 13:20:52 +0300

    Created a form for user to enter theirpitch

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,3 +8,9 @@ from wtforms import ValidationError
 class UpdateProfile(FlaskForm):
 	bio = TextAreaField("Tell us about you.", validators=[Required()])
 	submit = SubmitField('submit')
+
+
+class PitchForm(FlaskForm):
+	body = TextAreaField("whats your pitch ?",validators=[Required()])
+	category = RadioField('Label', choices=[('pickup lines','pickup lines'), ('interview pitch','interview pitch'),('product pitch','product pitch'),('promotion pitch','promotion pitch')])
+	submit = SubmitField('Submit')
