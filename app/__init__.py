edc1aa4998c834969164b6a76214952a73593531
maxwellharon <maxwellharon@gmail.com>
2018-06-24 13:41:54 +0300

    Created a function whereby the app is initialized when called in the manage.py file

--- app/__init__.py
+++ app/__init__.py
@@ -24,3 +24,44 @@ moment = Moment()
 login_manager = LoginManager()
 login_manager.session_protection='strong'
 login_manager.login_view = 'auth.login'
+
+
+def create_app(config_name):
+	"""this function initializes the app when called from manage.py file"""
+
+	app = Flask(__name__)
+	app.config.from_object(config_options[config_name])
+
+	# database initialization
+	db.init_app(app)
+
+	# bootstrap
+	bootstrap.init_app(app)
+
+	# bcrypt
+	bcrypt.init_app(app)
+
+	# login
+	login_manager.init_app(app)
+
+	# blueprints
+	from .auth import auth as auth_blueprint
+	app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
+
+	# photos configurations
+	configure_uploads(app,photos)
+
+    #simple markdown initialization
+	simple.init_app(app)
+
+	#socketio initialization
+	socketio.init_app(app)
+
+	#flask moment initialization
+	moment.init_app(app)
+
+	# import and register blueprints
+	from .main import main as main_blueprint
+	app.register_blueprint(main_blueprint)
+
+	return app
