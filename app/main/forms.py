fcde640be4ea2935e9abe18e300f001ebfcc79e5
maxwellharon <maxwellharon@gmail.com>
2018-06-24 13:22:14 +0300

    Added a class for the cooment section

--- app/main/forms.py
+++ app/main/forms.py
@@ -14,3 +14,9 @@ class PitchForm(FlaskForm):
 	body = TextAreaField("whats your pitch ?",validators=[Required()])
 	category = RadioField('Label', choices=[('pickup lines','pickup lines'), ('interview pitch','interview pitch'),('product pitch','product pitch'),('promotion pitch','promotion pitch')])
 	submit = SubmitField('Submit')
+
+
+
+class CommentForm(FlaskForm):
+	body = TextAreaField('',validators=[Required()])
+	submit = SubmitField()
