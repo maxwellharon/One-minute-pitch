6a1eb1cbe1ea99d247efc5749e50e2cec106e045
maxwellharon <maxwellharon@gmail.com>
2018-06-24 13:11:45 +0300

    Create a class Login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -13,3 +13,10 @@ class RegistrationForm(FlaskForm):
     EqualTo('password_confirm',message = 'Passwords do not match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+	email = StringField('Your Email Address', validators=[Required(),Email()])
+	password = PasswordField('Password', validators=[Required()])
+	remember = BooleanField('Remember me')
+	submit = SubmitField('Sign In')
