c5520e1e211b018f47df81a08f3352b5890c80bb
maxwellharon <maxwellharon@gmail.com>
2018-06-24 13:25:27 +0300

    Add a database for login functionalities

--- app/main/views.py
+++ app/main/views.py
@@ -6,3 +6,23 @@ from ..models import *
 from .forms import *
 from .. import db,photos
 from .. import socketio,send
+
+
+
+class IndexView(View):
+	decorators=[login_required,]
+
+	def __init__(self,template_name):
+		self.template_name = template_name
+
+	def dispatch_request(self):
+		form = PitchForm()
+		if form.validate_on_submit():
+			pitch = Pitch(body=form.body.data, author=current_user._get_current_object(),category=form.category.data)
+			db.session.add(pitch)
+			db.session.commit()
+			return redirect(url_for('main.index'))
+		pitches = Pitch.query.order_by(Pitch.timestamp.desc()).all()
+
+
+		return render_template(self.template_name,form=form, pitches=pitches)
