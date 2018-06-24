38a7f91ca45547551d2a89609b9b86b43cb4e3da
maxwellharon <maxwellharon@gmail.com>
2018-06-24 13:36:49 +0300

    Created an error handling service

--- app/main/views.py
+++ app/main/views.py
@@ -139,3 +139,17 @@ def downvote(id):
 	db.session.commit()
 
 	return redirect(url_for('main.index'))
+
+@socketio.on("message")
+def handleMessage(msg):
+	print("message " +msg)
+	send(msg,broadcast=True)
+
+
+@main.app_errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'),404
+
+@main.app_errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'),500
