2bb284d567bb7061c0a769bd053d8aed03b28fd4
maxwellharon <maxwellharon@gmail.com>
2018-07-03 19:08:30 +0300

    adding stuff

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maxwell:1234@localhost/piches'
     SECRET_KEY=os.environ.get("SECRET_KEY")
 
 
