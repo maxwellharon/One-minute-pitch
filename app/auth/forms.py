2bb7cd3aa8a1c03f0951ce7db67050f05858855b
maxwellharon <maxwellharon@gmail.com>
2018-07-03 17:27:46 +0300

    Forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,22 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField,BooleanField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
-from wtforms import ValidationError
 from ..models import User
-from wtforms.fields.html5 import EmailField
+from wtforms import ValidationError
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
 
 
 class RegistrationForm(FlaskForm):
-    email = EmailField('Your Email Address',validators=[Required(),Email(message="Email is required.")])
+    email = StringField('Your Email Address',validators=[Required(),Email()])
     username = StringField('Enter your username',validators = [Required()])
-    password = PasswordField('Password',validators = [Required(),
-    EqualTo('password_confirm',message = 'Passwords do not match')])
+    password = PasswordField('Password', validators ={Required(),
+                                                      EqualTo('password_confirm', message='Passwords must match')})
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
 
+    def validate_email(self, data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self, data_field):
+        if User.query.filter_by(username=data_field.data).first():
+            raise ValidationError('That username is taken')
+
 
 class LoginForm(FlaskForm):
-	email = StringField('Your Email Address', validators=[Required(),Email()])
-	password = PasswordField('Password', validators=[Required()])
-	remember = BooleanField('Remember me')
-	submit = SubmitField('Sign In')
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+
